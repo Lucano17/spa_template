a7101a203772339d9a116b58a50dfa24ef28af1f
Lucas De La Fuente <lucasdlf17@gmail.com>
2024-04-27 10:13:34 -0300

    padding-top 10px

--- src/Screens/Contact.jsx
+++ src/Screens/Contact.jsx
@@ -2,9 +2,6 @@ import React from 'react'
 import styles from "./Contact.module.css"
 
 const Contact = () => {
-
-
-
   return (
     <div name="Contact" className={styles.contact}>
       <h2>Contact</h2>
